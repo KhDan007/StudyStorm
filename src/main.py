a18f0a6210fc20f4ce21b3d887571fa7bcdc9768
KhDan007 <kh.dan007@gmail.com>
2023-04-08 17:07:42 +0600

    Comment everything

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
+# Import libraries
 from tkinter import *
-from PIL import Image, ImageTk, ImageDraw
 
+# Set constants
 WINDOW_WIDTH = 850
 WINDOW_HEIGHT = 600
 FRAME_WIDTH = WINDOW_WIDTH-188-188
 
+# Initialize the window
 window = Tk()
 window.title("StudyStorm | Login")
 window.resizable(False, False)
 window.geometry("{}x{}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
 window.config(background="#17255A")
 
+# Create frame
 frame1 = Frame(window, width=FRAME_WIDTH, height=WINDOW_HEIGHT, bg="#18206F")
 frame1.place(x=188, y=0)
 
+# Create logo label
 try:
     logo_photo = PhotoImage(file="./img/StudyStorm.png")
 except TclError:
@@ -23,5 +27,9 @@ else:
                        bg="#17255A", width=163, height=44, compound=CENTER)
     logo_label.place(x=156, y=72)
 
+# Create login label
+login_label = Label(frame1, text="Login:", font=('Lato', 25), fg="#ffffff", bg="#18206F")
+login_label.place(x=204, y=139)
 
+# Main loop
 window.mainloop()
