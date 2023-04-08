0e8155ec7b39539e31c733688247f9cb75a219d7
KhDan007 <kh.dan007@gmail.com>
2023-04-08 13:07:45 +0600

    create frame and add details

--- src/main.py
+++ src/main.py
@@ -4,7 +4,14 @@ WINDOW_WIDTH = 850
 WINDOW_HEIGHT = 600
 
 window = Tk()
+window.title("StudyStorm | Login")
+window.resizable(False, False)
 window.geometry("{}x{}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
-window.config(background="#18206F")
+window.config(background="#17255A")
+
+frame1 = Frame(window, width=WINDOW_WIDTH-188-188, height=WINDOW_HEIGHT, bg="#18206F",)
+frame1.pack(anchor=CENTER, expand=Y, fill="y")
+
+
 
 window.mainloop()
