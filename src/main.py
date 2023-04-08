562b8e04ca20ab5e04f44acb625855ac905133b2
KhDan007 <kh.dan007@gmail.com>
2023-04-08 17:05:49 +0600

    Create logo label

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from tkinter import *
+from PIL import Image, ImageTk, ImageDraw
 
 WINDOW_WIDTH = 850
 WINDOW_HEIGHT = 600
+FRAME_WIDTH = WINDOW_WIDTH-188-188
 
 window = Tk()
 window.title("StudyStorm | Login")
@@ -9,9 +11,17 @@ window.resizable(False, False)
 window.geometry("{}x{}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
 window.config(background="#17255A")
 
-frame1 = Frame(window, width=WINDOW_WIDTH-188-188, height=WINDOW_HEIGHT, bg="#18206F",)
-frame1.pack(anchor=CENTER, expand=Y, fill="y")
+frame1 = Frame(window, width=FRAME_WIDTH, height=WINDOW_HEIGHT, bg="#18206F")
+frame1.place(x=188, y=0)
 
+try:
+    logo_photo = PhotoImage(file="./img/StudyStorm.png")
+except TclError:
+    print("File not found!")
+else:
+    logo_label = Label(frame1, text="", image=logo_photo,
+                       bg="#17255A", width=163, height=44, compound=CENTER)
+    logo_label.place(x=156, y=72)
 
 
 window.mainloop()
