697553afe8c2e4d64b0e8cdae989e562dbe279bd
KhDan007 <kh.dan007@gmail.com>
2023-04-08 12:56:07 +0600

    initialize basic first window

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+WINDOW_WIDTH = 850
+WINDOW_HEIGHT = 600
+
+window = Tk()
+window.geometry("{}x{}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
+window.config(background="#18206F")
+
+window.mainloop()
