c56acfb809469d48cbf755217225526c125cd091
KhDan007 <kh.dan007@gmail.com>
2023-09-11 00:53:20 +0600

    creating css file and setting up the fxml file in SceneBuilder

--- src/App.java
+++ src/App.java
@@ -1,13 +1,16 @@
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, 850, 600);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginScene.fxml")));
+        Scene scene = new Scene(root);
 
         stage.setTitle("StudyStorm");
         stage.setScene(scene);
